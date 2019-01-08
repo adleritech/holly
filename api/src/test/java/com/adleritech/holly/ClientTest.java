10f834955c53ad24e455b49b3e2472001777715b
Radek Los <radek.los@gmail.com>
2019-01-08 10:51:21 +0100

    Git ignore and don't run client test

--- api/src/test/java/com/adleritech/holly/ClientTest.java
+++ api/src/test/java/com/adleritech/holly/ClientTest.java
@@ -1,6 +1,7 @@
 package com.adleritech.holly;
 
 import com.adleritech.holly.dto.Message;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -8,6 +9,7 @@ import java.io.IOException;
 
 public class ClientTest
 {
+    @Ignore
     @Test
     public void sendMessageToChannel() throws IOException {
         ClientFactory.createService("http://localhost:8000").sendMessage(new Message("_tests", "a\n```b```")).execute();
